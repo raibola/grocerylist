f9d950062e3215783b1e075ae2d766bbee435123
Raissa Bola <raissabola@gmail.com>
2019-06-07 20:45:49 -0700

    Views

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -8,15 +8,16 @@ describe("routes : static", () => {
   describe("GET /", () => {
 
 //#2
-    it("should return status code 200", (done) => {
+    it("should return status code 200 and have 'Welcome to Grocery List' in the body of the response", (done) => {
 
 //#3
       request.get(base, (err, res, body) => {
         expect(res.statusCode).toBe(200);
+        expect(body).toContain("Welcome to Grocery List");
 
 //#4
         done();
-      });
+      })
     });
 
   });
